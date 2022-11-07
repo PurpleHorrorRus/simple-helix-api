bfa319e4151e762283d3dc912a61215adc7f5dcc
Maxim <purplehorrorrus@gmail.com>
2022-11-07 17:15:06 +0800

    New endpoint: chatters

--- src/lib/requests/other.ts
+++ src/lib/requests/other.ts
@@ -11,6 +11,18 @@ class Other extends Static {
         user = user.toLowerCase();
         return await this.request(`https://tmi.twitch.tv/group/user/${user}/chatters`);
     }
+
+    async chatters(broadcaster_id: number, moderator_id: number, params = {}) { 
+        return await this.requestEndpoint("chat/chatters", {
+            broadcaster_id,
+            moderator_id,
+            ...params
+        });
+    }
+
+    async allChatters(broadcaster_id: number, moderator_id: number) { 
+        return await this.requestAll([broadcaster_id, moderator_id], this, "chatters", Infinity, 1000);
+    }
 }
 
 export default Other;
--- types/lib/requests/other.d.ts
+++ types/lib/requests/other.d.ts
@@ -3,5 +3,7 @@ import Static from "../static";
 declare class Other extends Static {
     constructor(headers: AxiosRequestHeaders);
     getViewers(user: string): Promise<any>;
+    chatters(broadcaster_id: number, moderator_id: number, params?: {}): Promise<any>;
+    allChatters(broadcaster_id: number, moderator_id: number): Promise<any>;
 }
 export default Other;
